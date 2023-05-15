6024e0ef9f25cb51222df6ec37063af8ff704857
duc.nguyenhuynh <duc.nguyenhuynh@fss.com.vn>
2023-05-15 21:38:56 +0700

    update

--- Less12/Js/home.js
+++ Less12/Js/home.js
@@ -30,11 +30,169 @@ var listItem = [
         content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
         image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
     },
+    {
+        id: 1,
+        name : "C++",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/java_web.jpg",
+    },{
+        id: 2,
+        name : "devmaster",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    },{
+        id: 3,
+        name : "IT",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/java_web.jpg",
+    },{
+        id: 4,
+        name : "ASP NET",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    },{
+        id: 5,
+        name : "NET CORE",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/java_web.jpg",
+    },{
+        id: 6,
+        name : "Salary",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    },
+    {
+        id: 1,
+        name : "C++",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/java_web.jpg",
+    },{
+        id: 2,
+        name : "devmaster",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    },{
+        id: 3,
+        name : "IT",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/java_web.jpg",
+    },{
+        id: 4,
+        name : "ASP NET",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    },{
+        id: 5,
+        name : "NET CORE",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/java_web.jpg",
+    },{
+        id: 6,
+        name : "Salary",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    },{
+        id: 4,
+        name : "ASP NET",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    },{
+        id: 5,
+        name : "NET CORE",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/java_web.jpg",
+    },{
+        id: 6,
+        name : "Salary",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    },
+    {
+        id: 1,
+        name : "C++",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/java_web.jpg",
+    },{
+        id: 2,
+        name : "devmaster",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    },{
+        id: 3,
+        name : "IT",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/java_web.jpg",
+    },{
+        id: 4,
+        name : "ASP NET",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    },{
+        id: 5,
+        name : "NET CORE",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/java_web.jpg",
+    },{
+        id: 6,
+        name : "Salary",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    },{
+        id: 4,
+        name : "ASP NET",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    },{
+        id: 5,
+        name : "NET CORE",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/java_web.jpg",
+    },{
+        id: 6,
+        name : "Salary",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    },
+    {
+        id: 1,
+        name : "C++",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/java_web.jpg",
+    },{
+        id: 2,
+        name : "devmaster",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    },{
+        id: 3,
+        name : "IT",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/java_web.jpg",
+    },{
+        id: 4,
+        name : "ASP NET",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    },{
+        id: 5,
+        name : "NET CORE",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/java_web.jpg",
+    },{
+        id: 6,
+        name : "Salary",
+        content : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        image : "https://devmaster.edu.vn/uploads/images/course/android.jpg",
+    }
 ]
-
-function setItem(arr){
+var pageSize = 5;
+var pageIndex = 1;
+function setItem(arr, pageIndex){
+    debugger;
     $("#list-items").html('');
-    for(var  i = 0; i < arr.length ; i++){
+    var start = (pageIndex > 1) ? (pageSize * (pageIndex - 2) + pageSize) : 0;
+    var end =   pageSize * (pageIndex - 1) + pageSize;
+    for(var  i = start; i < end && i < arr.length; i++){
         renderItem(arr[i]);
     }
 }
@@ -53,6 +211,53 @@ function renderItem(obj){
 }
 
 $(document).ready(function(){
-    setItem(listItem);
+    setItem(listItem , pageIndex);
 
 })
+
+var totalPage = listItem.length / pageSize;
+if(listItem.length % pageSize != 0){
+    totalPage++;
+}
+
+function setPage(totalPage){
+    $('#listPage').html('');
+    for(var  i = 1; i <= totalPage ; i++){
+        let page = `<div class="mr-3 bg-black">
+                        <span onclick="setItem2(${i})">${i}</span>
+                    </div>`;
+        $('#listPage').append(page);
+    }
+}   
+setPage(totalPage);
+function setItem2(pageIndex){
+    debugger;
+    // $("#list-items").html('');
+    var start = (pageIndex > 1) ? (pageSize * (pageIndex - 2) + pageSize) : 0;
+    var end =   pageSize * (pageIndex - 1) + pageSize;
+    for(var  i = start; i < end && i < listItem.length; i++){
+        renderItem(listItem[i]);
+    }
+}
+// arr: 30 data
+// 1 page: size 5 => 30 / 5 = 6 page
+
+
+// pageSize * (pageindex - 1) + pageSize
+
+//page 1: lấy các phần có index 0 - 4   >>> for từ 0 cho tới < 5   ===> start 0 ; end  = 5
+//page 2: lấy các phần có index 5 - 10  >>> for từ 5 cho tới < 10   ==> start 5 cho tới < (start + 5) : 5 +=> 10
+//page 3: lấy các phần có index 10 - 14     for từ 10 cho tới < 15  ==> start 10 cho tới 15
+//page 4: lấy các phần có index 15 - 19>>   for từ 15 cho tới < 20
+//page 5: lấy các phần có index 15 - 19>>   for từ 20 cho tới < 25
+//page 6: lấy các phần có index 15 - 19>>   for từ 25 cho tới < 30
+
+// start2 = end1;
+// end2 = start2 + 5;
+
+
+// start3 = end2;
+// end3 =  start3 + 5 ===> end3 = end2 + 5 ;
+
+
+
